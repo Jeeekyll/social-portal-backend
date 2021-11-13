01d663938ecdfd98e51ef8c5ad7dde6418353e15
Evgeniy <poddubniy209@gmail.com>
2021-11-13 11:36:22 +0300

    prettier issues

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -3,7 +3,7 @@ import { ConnectionOptions } from 'typeorm';
 const config: ConnectionOptions = {
   type: 'postgres',
   host: 'localhost',
-  port: 5433,
+  port: 5432,
   username: 'postgres',
   password: 'root',
   database: 'nestjs_realworld',
